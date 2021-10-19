489a425232d3890bfcf53d958bf2678d85e700c4
Joao Euko <joaopeuko@gmail.com>
2021-10-19 10:11:08 -0300

    max_values added to server side to /weather?max=<max_number>

--- server.py
+++ server.py
@@ -1,6 +1,5 @@
-from flask import Flask
+from flask import Flask, request
 from lib.openweather import OpenWeather
-
 
 app = Flask(__name__)
 weather = OpenWeather()
@@ -10,6 +9,16 @@ weather = OpenWeather()
 def results(city_name):
     result = weather.get_weather_by_city(city_name)
     return result
+
+
+@app.route('/weather', methods=['GET'])
+def max_values():
+    default = 5
+    print(len(request.args))
+    if not request.args or not request.args['max'].isnumeric():
+        return str(default)
+    else:
+        return request.args['max']
 
 
 if __name__ == '__main__':
