1eb2f772784d5843251ebc943f53a2535f3da95a
Joao Euko <joaopeuko@gmail.com>
2021-10-20 14:32:52 -0300

    Server fixed running.

--- server.py
+++ server.py
@@ -1,13 +1,24 @@
+import json
 from flask import Flask, request
 from lib.openweather import OpenWeather
+from flask_caching import Cache
 
-COOKIE_EXPIRATION_TIME = 5  # minutes
-
+config = {
+    "DEBUG": True,  # some Flask specific configs
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 5 * 60
+}
 app = Flask(__name__)
+# tell Flask to use the above defined config
+app.config.from_mapping(config)
+
+cache = Cache(app)
+
 weather = OpenWeather()
 
 
 @app.route('/weather/<city_name>')
+@cache.cached()
 def results(city_name):
     result = weather.get_weather_by_city(city_name)
     return result
@@ -15,12 +26,15 @@ def results(city_name):
 
 @app.route('/weather', methods=['GET'])
 def max_values():
-    default = 5
-    if not request.args or not request.args['max'].isnumeric():
-        return str(default)
+    if request.args and request.args['max'].isnumeric():
+        max_number = int(request.args['max'])
     else:
-        result = request.args['max']
-        return result
+        max_number = 5
+
+    result = []
+    for k in reversed(list(cache.cache._cache.keys())[-max_number:]):
+        result.append(cache.get(k))
+    return json.dumps(result)
 
 
 if __name__ == '__main__':
